abdc66bfff5ea13d02cc95d80c0fe34477e6eb49
yudai miyaguni <s19017@std.it-college.ac.jp>
2020-01-21 16:59:18 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,45 +1,52 @@
-import React from 'react'
+import React, {Component} from 'react'
 
-const TestView = props => <div>{props.value}</div>
-
-class TestForm extends React.Component {
+export default class MultiForm extends Component {
   constructor (props) {
     super(props)
-    this.state = { value: '' }
+    this.state = {
+      name: 'クジラ',
+      age: 22,
+      hobby: '読書'
+    }
   }
-
-  handleChange (event) {
-    this.setState({ value: event.target.value })
+  doChange (e) {
+    const userValue = e.target.value
+    const key = e.target.name
+    this.setState({[key]: userValue})
   }
-
+  doSubmit (e) {
+    e.preventDefault()
+    const j = JSON.stringify(this.state)
+    window.alert(j)
+  }
   render () {
+    const doSubmit = (e) => this.doSubmit(e)
+    const doChange = (e) => this.doChange(e)
     return (
-      <form onSubmit={(e, v) => this.props.update(e, this.state.value)}>
-        <input type='text' onChange={e => this.handleChange(e)} />
-        <input type='submit' />
+      <form onSubmit={doSubmit}>
+        <div><label>
+          名前: <br />
+          <input name='name'
+            type='text'
+            value={this.state.name}
+            onChange={doChange} />
+        </label></div>
+        <div><label>
+          年齢: <br />
+          <input name='age'
+            type='number'
+            value={this.state.age}
+            onChange={doChange} />
+        </label></div>
+        <div><label>
+          趣味: <br />
+          <input name='hobby'
+            type='text'
+            value={this.state.hobby}
+            onChange={doChange} />
+        </label></div>
+        <input type='submit' value='送信' />
       </form>
     )
   }
 }
-
-class App extends React.Component {
-  constructor (props) {
-    super(props)
-    this.state = { value: 'first' }
-  }
-
-  handleSubmit (e, value) {
-    e.preventDefault()
-    this.setState({ value: value })
-  }
-  render () {
-    return (
-      <div>
-        <TestView value={this.state.value} />
-        <TestForm update={this.handleSubmit.bind(this)} />
-      </div>
-    )
-  }
-}
-
-export default App
