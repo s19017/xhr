d420e2ff583cbd03ce515004812751d20f6d8042
yudai miyaguni <s19017@std.it-college.ac.jp>
2020-01-28 16:09:38 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React, {Component} from 'react'
+import React from 'react'
 
-export default class MultiForm extends Component {
+class MultiForm extends React.Component {
   constructor (props) {
     super(props)
     this.state = {
@@ -8,45 +8,77 @@ export default class MultiForm extends Component {
       age: 22,
       hobby: '読書'
     }
+    this.doSubmit = this.doSubmit.bind(this)
+    this.doChange = this.doChange.bind(this)
   }
+
   doChange (e) {
     const userValue = e.target.value
     const key = e.target.name
-    this.setState({[key]: userValue})
+    this.setState({ [key]: userValue })
   }
+
   doSubmit (e) {
     e.preventDefault()
     const j = JSON.stringify(this.state)
     window.alert(j)
   }
+
   render () {
-    const doSubmit = (e) => this.doSubmit(e)
-    const doChange = (e) => this.doChange(e)
     return (
-      <form onSubmit={doSubmit}>
-        <div><label>
-          名前: <br />
-          <input name='name'
-            type='text'
-            value={this.state.name}
-            onChange={doChange} />
-        </label></div>
-        <div><label>
-          年齢: <br />
-          <input name='age'
-            type='number'
-            value={this.state.age}
-            onChange={doChange} />
-        </label></div>
-        <div><label>
-          趣味: <br />
-          <input name='hobby'
-            type='text'
-            value={this.state.hobby}
-            onChange={doChange} />
-        </label></div>
+      <form action='' onSubmit={this.doSubmit}>
+        <div>
+          <label>
+            名前: <br />
+            <input
+              name='name'
+              type='text'
+              value={this.state.name}
+              onChange={this.doChange}
+            />
+          </label>
+        </div>
+        <div>
+          <label>
+            年齢: <br />
+            <input
+              name='age'
+              type='number'
+              value={this.state.age}
+              onChange={this.doChange}
+            />
+          </label>
+        </div>
+        <div>
+          <label>
+            趣味: <br />
+            <input
+              name='hobby'
+              type='text'
+              value={this.state.hobby}
+              onChange={this.doChange}
+            />
+          </label>
+        </div>
         <input type='submit' value='送信' />
       </form>
     )
   }
 }
+
+class App extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = { value: '' }
+  }
+
+  render () {
+    return (
+      <div>
+        <MultiForm />
+      </div>
+    )
+  }
+}
+
+export default 
