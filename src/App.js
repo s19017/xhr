0d9e92fcdb1c91ee67ea687bac3eaa89a39e8264
yudai miyaguni <s19017@std.it-college.ac.jp>
2020-01-28 16:14:37 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,65 @@
 import React from 'react'
+import ValueInput from './ValueInput'
 
-const Greeting = props => <li>{props.type}</li>
+class InchToCm extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      inch: 0,
+      cm: 0
+    }
+    this.inchChanged = this.inchChanged.bind(this)
+    this.cmChanged = this.cmChanged.bind(this)
+  }
+
+  inchChanged (e) {
+    const inchValue = e.value
+    const cmValue = inchValue * 2.54
+    this.setState({
+      inch: inchValue,
+      cm: cmValue
+    })
+  }
+
+  cmChanged (e) {
+    const cmValue = e.value
+    const inchValue = cmValue / 2.54
+    this.setState({
+      inch: inchValue,
+      cm: cmValue
+    })
+  }
+
+  render () {
+    return (
+      <div>
+        <ValueInput
+          title='inch'
+          onChange={e => this.inchChanged(e)}
+          value={this.state.inch}
+        />
+        <ValueInput
+          title='cm'
+          onChange={e => this.cmChanged(e)}
+          value={this.state.cm}
+        />
+      </div>
+    )
+  }
+}
 
 class App extends React.Component {
   constructor (props) {
     super(props)
-    this.state = { data: [] }
+    this.state = { value: '' }
   }
 
   render () {
-    const greetings = this.state.data.map((v, i) => (
-      <Greeting type={v} key={i} />
-    ))
-
-    return <ul>{greetings}</ul>
-  }
-
-  componentDidMount () {
-    fetch('./messages.json')
-      .then(res => res.json())
-      .then(json => json.messages)
-      .then(json => this.setState({ data: json }))
+    return (
+      <div>
+        <InchToCm />
+      </div>
+    )
   }
 }
 
