c0c572e07d7b16389e5a928379314ca82fc4ba3f
yudai miyaguni <s19017@std.it-college.ac.jp>
2020-01-28 16:16:31 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,50 +1,42 @@
 import React from 'react'
-import ValueInput from './ValueInput'
 
-class InchToCm extends React.Component {
+class ColorBox extends React.Component {
   constructor (props) {
     super(props)
     this.state = {
-      inch: 0,
-      cm: 0
+      index: 0,
+      colors: ['red', 'blue', 'green', 'yellow', 'black']
     }
-    this.inchChanged = this.inchChanged.bind(this)
-    this.cmChanged = this.cmChanged.bind(this)
-  }
-
-  inchChanged (e) {
-    const inchValue = e.value
-    const cmValue = inchValue * 2.54
-    this.setState({
-      inch: inchValue,
-      cm: cmValue
-    })
-  }
-
-  cmChanged (e) {
-    const cmValue = e.value
-    const inchValue = cmValue / 2.54
-    this.setState({
-      inch: inchValue,
-      cm: cmValue
-    })
   }
 
   render () {
-    return (
-      <div>
-        <ValueInput
-          title='inch'
-          onChange={e => this.inchChanged(e)}
-          value={this.state.inch}
-        />
-        <ValueInput
-          title='cm'
-          onChange={e => this.cmChanged(e)}
-          value={this.state.cm}
-        />
-      </div>
-    )
+    const colors = this.state.colors
+    const index = this.state.index
+    const cur = index >= 0 ? colors[index] : ''
+    const items = colors.map(c => {
+      let bs = '1px solid white'
+      if (c === cur) {
+        bs = '1px solid black'
+      }
+      const cstyle = {
+        color: c,
+        border: bs
+      }
+      const clickHandler = e => {
+        const c = e.target.style.color
+        const i = this.state.colors.indexOf(c)
+        this.setState((index: i))
+        if (this.props.onColorChange) {
+          this.props.onColorChange({ color: c })
+        }
+      }
+      return (
+        <span onClick={clickHandler} style={cstyle}>
+          ■
+        </span>
+      )
+    })
+    return <div>{items}</div>
   }
 }
 
@@ -57,7 +49,7 @@ class App extends React.Component {
   render () {
     return (
       <div>
-        <InchToCm />
+        <ColorBox />
       </div>
     )
   }
